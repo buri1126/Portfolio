83a4cc38353f49b69de0c9451bb4cb73bd385344
buri1126 <koutaroh1126@docomo.ne.jp>
2023-08-07 13:53:50 +0900

    App CSS レスポンシブ化　edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,35 +6,35 @@
     </head>
     <x-app-layout>
     <body>
-        <div class="edit_profile_component">
-            <h1>プロフィール編集</h1>
+        <div class="edit_profile_component h-screen w-3/4 lg:w-1/2 ">
+            <h1 class="text-center">プロフィール編集</h1>
             <form action="/users/{{$user->id}}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class="user_name">
                     <h2>{{$user->name}}</h2>
                 </div>
-                <div class="favorite">
-                     <div class="favoriteplayer">
+                <div class="favorite flex justify-center">
+                     <div class="favoriteplayer mx-5">
                         <h2>favoriteplayer</h2>
                         <input type="text" name="user[favoritePlayer]" placeholder="選手名" value="{{ $user->favoritePlayer }}"/>
                     </div>
-                    <div class="favoriteteam">
+                    <div class="favoriteteam mx-5">
                         <h2>favorite team</h2>
                         <input type="text" name="user[favoriteTeam]" placeholder="チーム名" value="{{ $user->favoriteTeam }}"/>
                     </div>
                    
                 </div>
                 <br>
-                <div class="info">
-                    <textarea name="user[info]">{{ $user->info }}</textarea>
+                <div class="info text-center">
+                    <textarea name="user[info]" class="w-1/2 h-25">{{ $user->info }}</textarea>
                 </div>
                 
-                <div class="save_profile">
+                <div class="save_profile text-center bg-green-700 text-white w-1/4">
                     <input type="submit" value="保存"/>
                 </div>
             </form>
-            <div class="footer">
+            <div class="footer text-center">
             <a href="/">back</a>
         </div>
         </div>
