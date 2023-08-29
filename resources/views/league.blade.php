2c25c74946b3888de8f810efd7ba91999af98304
buri1126 <koutaroh1126@docomo.ne.jp>
2023-08-29 22:05:45 +0900

    App ＣＳＳ　league 修正

--- resources/views/league.blade.php
+++ resources/views/league.blade.php
@@ -7,12 +7,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('/css/style.css')  }}">
+        <script   src="https://code.jquery.com/jquery-3.7.0.min.js"   integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="   crossorigin="anonymous"></script>
     </head>
     <x-app-layout>
         <body>
             <div class="league_component flex flex-col w-11/12 md:w-4/6 mt-16 h-screen">
                 <div class='standings text-center  w-3/4 mb-10'>
-                    <p class="text-3xl standings_title"><span>現在の順位</span></p>
+                    <div class="mb-3">
+                    <p class="text-3xl standings_title mb-3"><span>現在の順位</span></p>
+                    </div>
                     <table class="w-full bg-white">
                         <thead>
                             <tr>
@@ -44,8 +47,12 @@
                         </tbody>
                     </table>
                 </div>
-                <div class='fixtures text-center w-3/4'>
+                <div class='fixtures text-center w-3/4 this_Month'>
                     <p class="text-3xl fixtures_title"><span>今月の試合日程</span></p>
+                    <div class="fixture_button flex justify-between mb-3">
+                        <button onclick=prev()><i class="fa-solid fa-angles-left"></i></button>
+                        <button onclick=next()><i class="fa-solid fa-angles-right"></i></button>
+                    </div>
                     <table class="w-full bg-white">
                         <thead>
                             <tr class="border border-green-400 border-solid">
@@ -58,8 +65,8 @@
                         </thead>
                         <tbody>
                             <tr>
-                                @if($fixturedatas)
-                                   @foreach($fixturedatas as $fixture)
+                                @if($fixturedata)
+                                   @foreach($fixturedata as $fixture)
                                         <tr>
                                             <td class="border border-green-400 border-solid">{{substr($fixture['fixture']['date'],0,10)}}<br>{{substr($fixture['fixture']['date'],11,5)}}</td>
                                             <td class="border border-green-400 border-solid">{{substr($fixture['league']['round'],17,2)}}</td>
@@ -107,6 +114,150 @@
                                 @else
                                 <p>今日の試合はありません</p>
                                 @endif
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class='fixtures text-center w-3/4 prev_Month hidden'>
+                        <p class="text-3xl fixtures_title "><span>先月の試合日程</span></p>
+                    <div class="fixture_label flex justify-end mb-3">
+                        <button onclick=prevToThis()><i class="fa-solid fa-angles-right"></i></button>
+                    </div>
+                    @if($fixturedata_prev)
+                    <table class="w-full bg-white">
+                        <thead>
+                            <tr class="border border-green-400 border-solid">
+                            <th class="border border-green-400 border-solid">日付</th>
+                            <th class="border border-green-400 border-solid">節</th>
+                            <th class="border border-green-400 border-solid">ホーム</th>
+                            <th class="border border-green-400 border-solid">vs</th>
+                            <th class="border border-green-400 border-solid">アウェイ</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            <tr>
+                               @foreach($fixturedata_prev as $fixture)
+                                    <tr>
+                                        <td class="border border-green-400 border-solid">{{substr($fixture['fixture']['date'],0,10)}}<br>{{substr($fixture['fixture']['date'],11,5)}}</td>
+                                        <td class="border border-green-400 border-solid">{{substr($fixture['league']['round'],17,2)}}</td>
+                                        @if($fixture['teams']['home']['winner']===true)
+                                        <td class="border border-green-400 border-solid bg-white text-black">
+                                            <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                            {{$fixture['teams']['home']['name']}}
+                                            <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                        </td>
+                                        @elseif($fixture['teams']['home']['winner']===false)
+                                        <td class="border border-green-400 border-solid bg-black text-white">
+                                            <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                            {{$fixture['teams']['home']['name']}}
+                                            <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                            </td>
+                                        @else
+                                        <td class="border border-green-400 border-solid">
+                                            <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                            {{$fixture['teams']['home']['name']}}
+                                            <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                            </td>
+                                        @endif
+                                        <td class="border border-green-400 border-solid">vs</td>
+                                        @if($fixture['teams']['away']['winner']===true)
+                                        <td class="border border-green-400 border-solid bg-white text-black">
+                                            <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                            {{$fixture['teams']['away']['name']}}
+                                            <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                        </td>
+                                        @elseif($fixture['teams']['away']['winner']===false)
+                                        <td class="border border-green-400 border-solid bg-black text-white">
+                                            <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                            {{$fixture['teams']['away']['name']}}
+                                            <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                        </td>
+                                        @else
+                                        <td class="border border-green-400 border-solid">
+                                            <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                            {{$fixture['teams']['away']['name']}}
+                                            <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                        </td>
+                                        @endif
+                                    </tr>
+                               @endforeach
+                            </tr>
+                        </tbody>
+                    </table>
+                    @else
+                    <p>試合はありません</p>
+                     @endif
+                     
+                </div>
+                <div class='fixtures text-center w-3/4 next_Month hidden'>
+                   
+                        <p class="text-3xl fixtures_title"><span>来月の試合日程</span></p>
+                     <div class="fixture_button flex justify-start mb-3">
+                        <button onclick=nextToThis()><i class="fa-solid fa-angles-left"></i></button>
+                    </div>
+                    <table class="w-full bg-white">
+                        <thead>
+                            <tr class="border border-green-400 border-solid">
+                            <th class="border border-green-400 border-solid">日付</th>
+                            <th class="border border-green-400 border-solid">節</th>
+                            <th class="border border-green-400 border-solid">ホーム</th>
+                            <th class="border border-green-400 border-solid">vs</th>
+                            <th class="border border-green-400 border-solid">アウェイ</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            <tr>
+                                @if($fixturedata_next)
+                                   @foreach($fixturedata_next as $fixture)
+                                        <tr>
+                                            <td class="border border-green-400 border-solid">{{substr($fixture['fixture']['date'],0,10)}}<br>{{substr($fixture['fixture']['date'],11,5)}}</td>
+                                            <td class="border border-green-400 border-solid">{{substr($fixture['league']['round'],17,2)}}</td>
+                                            @if($fixture['teams']['home']['winner']===true)
+                                            <td class="border border-green-400 border-solid bg-white text-black">
+                                                <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                                {{$fixture['teams']['home']['name']}}
+                                                <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                            </td>
+                                            @elseif($fixture['teams']['home']['winner']===false)
+                                            <td class="border border-green-400 border-solid bg-black text-white">
+                                                <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                                {{$fixture['teams']['home']['name']}}
+                                                <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                                </td>
+                                            @else
+                                            <td class="border border-green-400 border-solid">
+                                                <img src="{{$fixture['teams']['home']['logo']}}" class="inline w-1/12">
+                                                {{$fixture['teams']['home']['name']}}
+                                                <span class="ml-2">{{$fixture['goals']['home']}}</span>
+                                                </td>
+                                            @endif
+                                            <td class="border border-green-400 border-solid">vs</td>
+                                            @if($fixture['teams']['away']['winner']===true)
+                                            <td class="border border-green-400 border-solid bg-white text-black">
+                                                <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                                {{$fixture['teams']['away']['name']}}
+                                                <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                            </td>
+                                            @elseif($fixture['teams']['away']['winner']===false)
+                                            <td class="border border-green-400 border-solid bg-black text-white">
+                                                <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                                {{$fixture['teams']['away']['name']}}
+                                                <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                            </td>
+                                            @else
+                                            <td class="border border-green-400 border-solid">
+                                                <span class="mr-2">{{$fixture['goals']['away']}}</span>
+                                                {{$fixture['teams']['away']['name']}}
+                                                <img src="{{$fixture['teams']['away']['logo']}}" class="inline w-1/12">
+                                            </td>
+                                            @endif
+                                        </tr>
+                                   @endforeach
+                                @else
+                                <p>今日の試合はありません</p>
+                                @endif
+                                
                             </tr>
                         </tbody>
                     </table>
@@ -116,4 +267,24 @@
             </div>
         </body>
     </x-app-layout>
+    <script>
+        function next(){
+            console.log("a");
+            $(".next_Month").toggleClass('hidden');
+            $(".this_Month").toggleClass('hidden');
+
+        };
+         function prev(){
+             $(".prev_Month").toggleClass('hidden');
+            $(".this_Month").toggleClass('hidden');
+        };
+        function nextToThis(){
+             $(".next_Month").toggleClass('hidden');
+            $(".this_Month").toggleClass('hidden');
+        };
+        function prevToThis(){
+             $(".prev_Month").toggleClass('hidden');
+            $(".this_Month").toggleClass('hidden');
+        };
+    </script>
 </html>
